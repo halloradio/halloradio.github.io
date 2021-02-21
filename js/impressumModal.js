691ed18428a222be7c58a9a07cf8fa76d34d70a5
Lasse Bornträger <la_bo@uni-bremen.de>
2021-02-21 13:36:24 +0100

    modal

--- js/impressumModal.js
+++ js/impressumModal.js
@@ -5,19 +5,32 @@ window.addEventListener('load', function () {
   // var modal = document.getElementById("secondModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementById("btnImp");
+  var btn = document.getElementById("xKanal");
+
+  var modalClose = document.getElementById("xKanalModal");
 
   // Get the <span> element that closes the modal
-  var span_close = document.getElementsByClassName("close")[0];
+  // var span_close = document.getElementsByClassName("close")[0];
+ 
+ 
+  document.getElementById("xKanalModal").classList.toggle("show");
+  document.getElementById("xKanal").classList.toggle("xActive");
+  // xkanalImg
+
+
+// modalClose.onClick = function(){
+//   document.getElementById("xKanalModal").classList.toggle("show");
+// }
 
 
 
   btn.onclick = function() {   
-    console.log("nabdwdkjandansdlkj")
-    document.getElementById("secondModal").classList.toggle("show");
-    
+    document.getElementById("xKanalModal").classList.toggle("show");
+    document.getElementById("xKanal").classList.toggle("xActive");
+    // document.getElementsByClassName(".xk").classList.toggle("xActive");
   }
-  span_close.onclick = function() {
-    document.getElementById("secondModal").classList.toggle("show");
+  modalClose.onclick = function() {
+    document.getElementById("xKanalModal").classList.toggle("show");
+    document.getElementById("xKanal").classList.toggle("xActive");
   }
 });
